1a420c5253761cde7522298a003f7a7e3c96aa09
Mathieu Gerard <mathieu.gerard@uclouvain.be>
2018-10-17 15:10:47 +0200

    IUFC-74 Reformat

--- admin.py
+++ admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from continuing_education.models import admission, continuing_education_person, address
 
-admin.site.register(admission.Admission, admission.AdmissionAdmin)
-admin.site.register(continuing_education_person.ContinuingEducationPerson,
-                    continuing_education_person.ContinuingEducationPersonAdmin)
-admin.site.register(address.Address, address.AddressAdmin)
+admin.site.register(
+    admission.Admission,
+    admission.AdmissionAdmin
+)
+admin.site.register(
+    continuing_education_person.ContinuingEducationPerson,
+    continuing_education_person.ContinuingEducationPersonAdmin
+)
+admin.site.register(
+    address.Address,
+    address.AddressAdmin
+)
