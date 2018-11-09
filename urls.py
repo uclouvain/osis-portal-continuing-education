1fb40b1245cf830b660e5671114b34d0c52e7224
BenJ <benjamin.daubry@uclouvain.be>
2018-11-09 16:15:49 +0100

    IUFC-99 : Url to view and download document

--- urls.py
+++ urls.py
@@ -56,5 +56,7 @@ urlpatterns = [
     url(r'^admission_detail/(?P<admission_id>[0-9]+)$', admission.admission_detail, name='admission_detail'),
     url(r'^registration_edit/(?P<admission_id>[0-9]+)$', registration.registration_edit, name='registration_edit'),
     url(r'^registration_detail/(?P<admission_id>[0-9]+)$', registration.registration_detail,
-        name='registration_detail')
+        name='registration_detail'),
+    url(r'^view_file/(?P<path>.*)$', admission.view_file, name="view_file"),
+    url(r'^download_file/(?P<path>.*)$', admission.download_file, name="download_file")
 ]
