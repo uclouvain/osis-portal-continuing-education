a4e64315b5bd1affbeec66bc2f48a21027328420
BenJ <benjamin.daubry@uclouvain.be>
2019-03-15 10:54:19 +0100

    Add email in registration view

--- views/registration.py
+++ views/registration.py
@@ -59,7 +59,7 @@ def registration_detail(request, admission_id):
     admission = get_object_or_404(Admission, pk=admission_id)
     if admission.state == admission_state_choices.REGISTRATION_SUBMITTED:
         add_remaining_tasks_message(request)
-        add_contact_for_edit_message(request, is_registration=True)
+        add_contact_for_edit_message(request, formation=admission.formation, is_registration=True)
     if admission.state == admission_state_choices.ACCEPTED:
         add_informations_message_on_submittable_file(
             request=request,
