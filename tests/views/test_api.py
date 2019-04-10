48a3e12b4b4723541b52b28a684dd3b5364dcb35
BenJ <benjamin.daubry@uclouvain.be>
2019-04-10 09:35:06 +0200

    Add tests

--- tests/views/test_api.py
+++ tests/views/test_api.py
@@ -23,12 +23,14 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
+import uuid
 from unittest import mock
 
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.test import TestCase, RequestFactory
 from rest_framework import status
+from rest_framework.exceptions import PermissionDenied
 
 from base.tests.factories.academic_year import create_current_academic_year, AcademicYearFactory
 from base.tests.factories.person import PersonFactory
@@ -36,7 +38,8 @@ from continuing_education.models.enums.admission_state_choices import SUBMITTED
 from continuing_education.tests.factories.admission import AdmissionDictFactory
 from continuing_education.tests.factories.continuing_education_training import ContinuingEducationTrainingDictFactory
 from continuing_education.tests.factories.person import ContinuingEducationPersonDictFactory
-from continuing_education.views.api import get_token_from_osis, get_personal_token
+from continuing_education.views.api import get_token_from_osis, get_personal_token, get_admission, \
+    get_registration, get_continuing_education_training, get_continuing_education_person
 
 
 class ApiMethodsTestCase(TestCase):
@@ -79,3 +82,59 @@ class ApiMethodsTestCase(TestCase):
         self.assertEqual(token, "token")
         self.assertEqual(self.request.session['personal_token'], "token")
         self.assertFalse(mock_post.called)
+
+    @mock.patch('continuing_education.views.api.get_personal_token')
+    @mock.patch('requests.get', return_value=HttpResponse(status=status.HTTP_404_NOT_FOUND))
+    def test_get_admission_not_found(self, mock_get, mock_token):
+        with self.assertRaises(Http404):
+            get_admission(self.request, uuid.uuid4())
+        self.assertTrue(mock_get.called)
+
+    @mock.patch('continuing_education.views.api.get_personal_token')
+    @mock.patch('requests.get', return_value=HttpResponse(status=status.HTTP_403_FORBIDDEN))
+    def test_get_admission_denied(self, mock_get, mock_token):
+        with self.assertRaises(PermissionDenied):
+            get_admission(self.request, uuid.uuid4())
+        self.assertTrue(mock_get.called)
+
+    @mock.patch('continuing_education.views.api.get_personal_token')
+    @mock.patch('requests.get', return_value=HttpResponse(status=status.HTTP_404_NOT_FOUND))
+    def test_get_registration_not_found(self, mock_get, mock_token):
+        with self.assertRaises(Http404):
+            get_registration(self.request, uuid.uuid4())
+        self.assertTrue(mock_get.called)
+
+    @mock.patch('continuing_education.views.api.get_personal_token')
+    @mock.patch('requests.get', return_value=HttpResponse(status=status.HTTP_403_FORBIDDEN))
+    def test_get_registration_denied(self, mock_get, mock_token):
+        with self.assertRaises(PermissionDenied):
+            get_registration(self.request, uuid.uuid4())
+        self.assertTrue(mock_get.called)
+
+    @mock.patch('continuing_education.views.api.get_personal_token')
+    @mock.patch('requests.get', return_value=HttpResponse(status=status.HTTP_404_NOT_FOUND))
+    def test_get_continuing_education_training_not_found(self, mock_get, mock_token):
+        with self.assertRaises(Http404):
+            get_continuing_education_training(self.request, uuid.uuid4())
+        self.assertTrue(mock_get.called)
+
+    @mock.patch('continuing_education.views.api.get_personal_token')
+    @mock.patch('requests.get', return_value=HttpResponse(status=status.HTTP_403_FORBIDDEN))
+    def test_get_continuing_education_training_denied(self, mock_get, mock_token):
+        with self.assertRaises(PermissionDenied):
+            get_continuing_education_training(self.request, uuid.uuid4())
+        self.assertTrue(mock_get.called)
+
+    @mock.patch('continuing_education.views.api.get_personal_token')
+    @mock.patch('requests.get', return_value=HttpResponse(status=status.HTTP_404_NOT_FOUND))
+    def test_get_continuing_education_person_not_found(self, mock_get, mock_token):
+        with self.assertRaises(Http404):
+            get_continuing_education_person(self.request)
+        self.assertTrue(mock_get.called)
+
+    @mock.patch('continuing_education.views.api.get_personal_token')
+    @mock.patch('requests.get', return_value=HttpResponse(status=status.HTTP_403_FORBIDDEN))
+    def test_get_continuing_education_person_denied(self, mock_get, mock_token):
+        with self.assertRaises(PermissionDenied):
+            get_continuing_education_person(self.request)
+        self.assertTrue(mock_get.called)
