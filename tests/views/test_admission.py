c9c5aa16f4e2aa9b6342be482638ba86b015a6d0
mathieuzen <zen.mathieu@gmail.com>
2021-09-30 15:31:34 +0200

    OSIS-6155 fix tests

--- tests/views/test_admission.py
+++ tests/views/test_admission.py
@@ -476,6 +476,7 @@ class AdmissionSubmissionErrorsTestCase(TestCase):
         self.assertDictEqual(
             errors,
             {
+                _("Email"): [_("This field is required.")],
                 _("Birth country"): [_("This field is required.")],
                 _("Postal code"): [_("This field is required.")],
                 _("Last degree level"): [_("This field is required.")]
