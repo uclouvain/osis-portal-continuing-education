0a6cd2ab9d78e1840e8389d20d7c45dadd4434eb
mathieuzen <zen.mathieu@gmail.com>
2021-10-14 16:32:13 +0200

    fix failing test continuing_education

--- tests/views/test_admission.py
+++ tests/views/test_admission.py
@@ -477,7 +477,6 @@ class AdmissionSubmissionErrorsTestCase(TestCase):
         self.assertDictEqual(
             errors,
             OrderedDict({
-                _("Email"): [_("This field is required.")],
                 _("Birth country"): [_("This field is required.")],
                 _("Postal code"): [_("This field is required.")],
                 _("Last degree level"): [_("This field is required.")]
